479c990a2ca139d9acf66ee1a3f8ac85c59d756a
Bakame1 <marko@danet.one>
2026-01-09 16:18:09 +0100

    2nd commit

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,10 +1,11 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @getMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
