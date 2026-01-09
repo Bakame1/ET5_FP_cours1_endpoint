9d6e13c292441e9153b37f575ba99afa0d1d6f22
Bakame1 <marko@danet.one>
2026-01-09 16:29:12 +0100

    doing http://localhost:8080/hello works

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -3,6 +3,8 @@ package controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// go to http://localhost:8080/hello
+
 @RestController
 public class HelloController {
     @GetMapping("/hello")
